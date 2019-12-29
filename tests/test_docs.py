6dda5b8436262097d365a79b46d87f36f297a5e8
Akshay Kathpal <Akshatkathpal29@gmail.com>
2019-12-29 14:09:49 +0530

    added test suite

---
+++ tests/test_docs.py
@@ -0,0 +1,26 @@
+import json ,logging , os , requests, unittest
+
+class TestAuth(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_intial_configuration(self):
+        pass
+
+    def test_check_connectivity(self):
+        pass
+
+    def test_authorization(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
